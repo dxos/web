64057159eaf30f96283702770a5116bbefab9b24
Zarco Nontol <zarconontol@gmail.com>
2021-12-15 18:29:33 -0300

    attempt to make build pass - webpack changes

--- plugins/webpack-plugin.js
+++ plugins/webpack-plugin.js
@@ -1,17 +1,57 @@
 // const webpack = require("webpack");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const path = require('path');
+const webpack = require('webpack');
 const { ConfigPlugin } = require('@dxos/config/ConfigPlugin');
 
 module.exports = function (context, options) {
   return {
     name: 'custom-docusaurus-plugin',
-    configureWebpack() {
+    configureWebpack(config) {
+      console.log('- config', config);
       return {
+        module: {
+          rules: [
+            // { 
+            //   test: /\.js$/, 
+            //   exclude: /node_modules/, 
+            //   loader: "babel-loader" 
+            // },
+            // {
+            //   test: /\.css$/i,
+            //   use: ['style-loader', 'css-loader'],
+            // },
+            {
+              test: /\.(png|svg|jpe?g|gif)$/,
+              include: /images/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/',
+                    publicPath: 'images/'
+                  }
+                }
+              ]
+            },
+          ]
+        },
+      
         plugins: [
           new ConfigPlugin({
             path: path.resolve(__dirname, 'config'),
             dynamic: process.env.CONFIG_DYNAMIC
+          }),
+          /**
+           * The package sodium-javascript, used on our packages, has a critical dependency issue.
+           * This issue is throwing a warning on the build output, and causing the CI to fail.
+           * The plugin below allows us to match the package during compilation and "acknowledge" the warning by ourselves.
+           * We are "acknowledging" every "critical" warning.
+           */
+          new webpack.ContextReplacementPlugin(/\/node_modules\//, (data) => {
+            data.dependencies.forEach(dependency => delete dependency.critical)
+            return data;
           }),
           new NodePolyfillPlugin()
         ],
@@ -20,6 +60,17 @@ module.exports = function (context, options) {
             fs: false
           },
         },
+        node: {
+          __dirname: true
+        },
+        externals: {
+          fatfs: 'fatfs',
+          runtimejs: 'runtimejs',
+          wrtc: 'wrtc',
+          bip32: 'bip32',
+          typeforce: 'typeforce',
+          'sodium-universal': 'sodium-universal'
+        }
       };
     },
   };
