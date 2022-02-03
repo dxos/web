d778eb3bb5bddb2b1c196cce5848eb42718cc6e4
Josiah Witt <josiah@witt.life>
2022-02-03 15:09:29 -0500

    fix: Disable dark mode

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+      },
       navbar: {
         logo: {
           alt: 'DXOS',
