faa5645d4e3c1337df105af0f46381ec94d4b39c
Josiah Witt <josiah@witt.life>
2022-02-14 12:14:50 -0500

    refactor: Factor out peers example (#47)

--- packages/demos/task-app/src/TaskApp.tsx
+++ packages/demos/task-app/src/TaskApp.tsx
@@ -2,16 +2,43 @@
 // Copyright 2021 DXOS.org
 //
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { ClientProvider } from '@dxos/react-client';
+import { Item } from '@dxos/client';
 import { TestFrameContainer } from '@dxos/frame-dev-server';
+import { ClientProvider, useTestPartyBootstrap } from '@dxos/react-client';
+import { DisplayMode } from '@dxos/react-framekit';
 import { manifest } from '@dxos/tasks-frame';
 
 export const Main = () => {
+  const peers = useTestPartyBootstrap();
+  const [item, setItem] = useState<Item<any>>();
+
+  useEffect(() => {
+    if (peers[0]?.party) {
+      setImmediate(async () => {
+        const item = await manifest.createRootItem?.(peers[0].party);
+        setItem(item);
+      });
+    }
+  }, [peers]);
+
+  if (!item) {
+    return null;
+  }
+
   return (
-    <ClientProvider>
-      <TestFrameContainer manifest={manifest} />
-    </ClientProvider>
+    <>
+      {peers.map(({ client, party }, index) => (
+        <ClientProvider key={index} client={client}>
+          <TestFrameContainer
+            manifest={manifest}
+            partyKey={party.key}
+            itemId={item.id}
+            mode={DisplayMode.COMPACT}
+          />
+        </ClientProvider>
+      ))}
+    </>
   )
 };
